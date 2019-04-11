31cb2d0374693574148188e5f3227d3913086201
“Kimberly <kimberly.achkio@gmail.com>
2019-04-11 14:28:03 -0600

    Add SpinMe component with initial styling

---
+++ src/components/SpinMe/SpinMe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SpinMe extends Component {
+  render () {
+    return (
+      <div className='spin-me-button-wrapper'>
+        <button className='spin-me-button'>Spin me to find your next tune</button>
+      </div>
+    )
+  }
+}
