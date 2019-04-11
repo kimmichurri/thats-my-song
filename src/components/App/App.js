7a8c5c3658cf30ae10bf78e21cef9fc0d02614ae
“Kimberly <kimberly.achkio@gmail.com>
2019-04-10 23:10:42 -0600

    Reorganize files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import './_App.scss';
 
 class App extends Component {
+
+  async componentDidMount() {
+    const url = `http://voiceboxpdx.com/api/v1/songs?by=popularity`
+    try {
+      const response = await fetch(url)
+      const songs = response.json()
+      console.log(songs)
+    } catch (error) {
+      throw new Error('There was a problem getting the data')
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div className="App">
         <header>
           <h1 className='main-title'>That's My Song</h1>
         </header>
