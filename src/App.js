c8377c8568fe8d321016d448bb969f921db00fbf
“Kimberly <kimberly.achkio@gmail.com>
2019-04-10 22:57:26 -0600

    Test fetch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  async componentDidMount() {
+    const url = `http://voiceboxpdx.com/api/v1/songs?by=popularity`
+    try {
+      const response = await fetch(url)
+      const songs = response.json()
+      console.log(songs)
+    } catch (error) {
+      throw new Error('There was a problem getting the data')
+    }
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './_App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1 className='main-title'>That's My Song</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
