076528137c1bcc49c19cb7a337338cec56771686
“Kimberly <kimberly.achkio@gmail.com>
2019-04-12 12:58:51 -0600

    Test mSTP for SongInfo.js

---
+++ src/components/SongInfo/SongInfo.test.js
@@ -0,0 +1,64 @@
+import { SongInfo, mapStateToProps } from './SongInfo';
+
+describe('SongInfo', () => {
+  describe('mapStateToProps', () => {
+    it('should return an array of songs', () => {
+      const mockState = {
+        isLoading: false,
+        setSongs: [
+          {
+            "title": "Don't Think Twice It's Alright",
+            "artist": "Dylan, Bob",
+            "tags": [
+                "Folk",
+                "Pop",
+                "Rock"
+            ]
+          },
+          {
+            "title": "There's A Light That Never Goes Out",
+            "artist": "Smiths, The",
+            "tags": [
+                "Alternative",
+                "Indie",
+                "Pop",
+                "Rock",
+                "80s",
+                "British"
+            ]
+          }
+        ],
+        otherThing: 'thing1',
+        anotherThing: ['thing2']
+      }
+      const expected = {
+        songs: [
+          {
+            "title": "Don't Think Twice It's Alright",
+            "artist": "Dylan, Bob",
+            "tags": [
+                "Folk",
+                "Pop",
+                "Rock"
+            ]
+          },
+          {
+            "title": "There's A Light That Never Goes Out",
+            "artist": "Smiths, The",
+            "tags": [
+                "Alternative",
+                "Indie",
+                "Pop",
+                "Rock",
+                "80s",
+                "British"
+            ]
+          }
+        ]
+      }
+      const mappedProps = mapStateToProps(mockState);
+      console.log(mappedProps);
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+});
