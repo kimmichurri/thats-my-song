6ea93f4da88f004332e5d0b0ece7067199bed4e3
“Kimberly <kimberly.achkio@gmail.com>
2019-04-12 11:39:15 -0600

    Test mSTP and mDTP in App.js

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { mapStateToProps, mapDispatchToProps } from './App';
+jest.mock('../../thunks/fetchSongs')
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  describe('mapStateToProps', () => {
+    it('should return a boolean to indicate if loading is true or false', () => {
+      const mockState = {
+        isLoading: false,
+        songs: ['song 1', 'song2'],
+        otherThing: 'thing1',
+        anotherThing: ['thing2']
+      }
+      const expected = {
+        loading: false
+      }
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should return fetchSongs to dispatch', () => {
+      const mockDispatch = jest.fn();
+      const fetchSongs = jest.fn();
+      const actionToDispatch = fetchSongs('www.songs.com');
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.fetchSongs('www.songs.com');
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+  });
 });
