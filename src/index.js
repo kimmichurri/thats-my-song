6e2493a97ba77fb0073e449e3378eb0ad90f1494
“Kimberly <kimberly.achkio@gmail.com>
2019-04-11 20:06:33 -0600

    Apply redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.scss';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
   , document.getElementById('root'));
 
 serviceWorker.unregister();
