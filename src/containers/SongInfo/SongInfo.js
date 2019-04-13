88232c56c170c687448a7253bf09c775f263ba9a
“Kimberly <kimberly.achkio@gmail.com>
2019-04-13 17:02:46 -0600

    Allow user to click to select random song from same category on SongInfo page

--- src/containers/SongInfo/SongInfo.js
+++ src/containers/SongInfo/SongInfo.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { fetchSongs } from '../../thunks/fetchSongs';
+import { setCurrentSong } from '../../actions/index';
 
 export class SongInfo extends Component {
+
+  reSpinCategory = (e) => {
+    let url;
+    const { value } = e.target
+    console.log(e.target.value);
+    switch (value) {
+      case 'Wildcard':
+        url = `http://voiceboxpdx.com/api/v1/songs/roulette`
+        console.log('wildcard', url);
+        this.props.fetchSongs(url)
+        break;
+      default:
+        url = `http://voiceboxpdx.com/api/v1/songs/roulette?tag=${value}`
+        console.log(url);
+        this.props.fetchSongs(url)
+    }
+  }
+
+  pickRandomSong = (e) => {
+    this.reSpinCategory(e);
+    let randomIndex = Math.floor(Math.random() * 10);
+    let randomSong = this.props.songs[randomIndex];
+    this.props.setCurrentSong(randomSong);
+  }
   
   render() {
     const { title, artist } = this.props.currentSong;
@@ -12,7 +38,10 @@ export class SongInfo extends Component {
         <h3 className='song-title'>{title}</h3>
         <p className='song-artist secondary'>{artist}</p>
         <button className='song-info-page-buttons'>Add to Playlist</button>
-        <button className='song-info-page-buttons'>Spin {this.props.currentCategory} Again</button>
+        <button 
+          className='song-info-page-buttons' 
+          value={this.props.currentCategory} 
+          onClick={this.pickRandomSong}>Spin {this.props.currentCategory} Again</button>
         <button className='song-info-page-buttons'>Return to Search</button>
       </div>
     )
@@ -26,8 +55,13 @@ SongInfo.propTypes = {
 
 export const mapStateToProps = (state) => ({
   currentCategory: state.setCurrentCategory,
-  currentSong: state.setCurrentSong
+  currentSong: state.setCurrentSong,
+  songs: state.setSongs
 });
 
+export const mapDispatchToProps = (dispatch) => ({
+  fetchSongs: (url) => dispatch(fetchSongs(url)),
+  setCurrentSong: (song) => dispatch(setCurrentSong(song))
+});
 
-export default connect(mapStateToProps, null)(SongInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(SongInfo);
