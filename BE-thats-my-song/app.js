bc6ef9b9f80f9489ac5a1e0cd59f6cb46ad0b90e
“Kimberly <kimberly.achkio@gmail.com>
2019-04-13 22:42:44 -0600

    Stub out pseudo code for POST request

--- BE-thats-my-song/app.js
+++ BE-thats-my-song/app.js
@@ -4,12 +4,20 @@ import cors from 'cors';
 app.use(cors());
 app.use(express.json());
 
-app.locals.playlist = [ { song: 'song1' }, { song: 'song2' }
+app.locals.playlist = [
 
 ];
 
 app.get('/api/v1/playlist/', (request, response) => {
   response.status(200).json(app.locals.playlist);
+});
+
+app.post('/api/v1/playlist/', (request, response) => {
+  //decide what to require from request.body
+  //mark it by its existing id
+  //send in the specific things as newPlaylistItem const from the body that you want, don't just spread it all in
+  //go ahead and push that newPlaylistItem into app.locals.playlist
+  //send back the proper response code with the newPlaylistItem(why this? Could we send a message saying "Jingle Bell Rock has successfully been added to your plaslist!")
 });
 
  export default app;
