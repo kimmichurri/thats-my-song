f17c5dd88f1eb106f76bc2601567521c5ead852f
“Kimberly <kimberly.achkio@gmail.com>
2019-04-13 23:29:32 -0600

    Create and implement route for playlist

--- FE-thats-my-song/src/containers/App/App.js
+++ FE-thats-my-song/src/containers/App/App.js
@@ -4,20 +4,34 @@ import { connect } from 'react-redux';
 import { fetchSongs } from '../../thunks/fetchSongs';
 import Loader from '../../components/Loader/Loader';
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import SongInfo from '../SongInfo/SongInfo';
+import headphones from '../../assets/headphones.png';
+import { Playlist } from '../Playlist/Playlist';
 
 
 export class App extends Component {
+
+  goToFavorites = () => {
+    console.log('do some playlist stuff now')
+  }
 
   render() {
     return (
       <div className="App">
         <header>
+          <NavLink to='/playlist'>
+            <button
+              onClick={this.goToFavorites}
+              className='playlist-button'>Playlist
+              <img className='headphones-icon' src={headphones} />
+            </button>
+          </NavLink>
           <h1 className='main-title'>That's My Song</h1>
         </header>
         <Route exact path='/' component={Selections} />
         <Route exact path='/song-info' component={SongInfo} />
+        <Route exact path='/playlist' component={Playlist} />
         {/* {this.props.loading ? <Loader /> : <Selections /> } */}
       </div>
     );
