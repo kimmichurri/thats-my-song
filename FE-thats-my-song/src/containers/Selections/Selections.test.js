3af4f46bb45b1311444ee92a109348b46946e196
“Kimberly <kimberly.achkio@gmail.com>
2019-04-14 19:51:06 -0600

    Test mSTP and mDTP

--- FE-thats-my-song/src/containers/Selections/Selections.test.js
+++ FE-thats-my-song/src/containers/Selections/Selections.test.js
@@ -1,7 +1,25 @@
-import { mapDispatchToProps } from './Selections';
+import { mapStateToProps, mapDispatchToProps } from './Selections';
 jest.mock('../../thunks/fetchSongs');
+import { setCurrentCategory, setCurrentSong } from '../../actions';
 
 describe('Selections', () => {
+  describe('mapStateToProps', () => {
+    it('should return songs and current category as props', () => {
+      const mockState = {
+        setSongs: [{song: 'a'}, {song: 'b'}],
+        setCurrentCategory: '90s',
+        otherThing: 'thing1',
+        anotherThing: ['thing2']
+      }
+      const expected = {
+        songs: [{song: 'a'}, {song: 'b'}],
+        currentCategory: '90s'
+      }
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
   describe('mapDispatchToProps', () => {
     it('should return fetchSongs to dispatch', () => {
       const mockDispatch = jest.fn();
@@ -9,6 +27,22 @@ describe('Selections', () => {
       const actionToDispatch = fetchSongs('www.songs.com');
       const mappedProps = mapDispatchToProps(mockDispatch);
       mappedProps.fetchSongs('www.songs.com');
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should return setCurrentCategory as props to dispatch', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = setCurrentCategory('90s');
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.setCurrentCategory('90s');
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should return setCurrentSong as props to dispatch', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = setCurrentSong({song: 'some song'});
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.setCurrentSong({song: 'some song'});
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
     });
   });
