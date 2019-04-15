376b8ad71e9f94ca6c775ba055ae0c52685309cc
“Kimberly <kimberly.achkio@gmail.com>
2019-04-15 14:21:59 -0600

    Update mock data

--- FE-thats-my-song/src/reducers/__tests__/setSongsReducer.js
+++ FE-thats-my-song/src/reducers/__tests__/setSongsReducer.js
@@ -15,33 +15,12 @@ describe('setSongsReducer', () => {
       {
         "id": 63303,
         "title": "Don't Think Twice It's Alright",
-        "artist": "Dylan, Bob",
-        "language": "English",
-        "popularity": 1398,
-        "play_count": 387,
-        "added_on": "2009-11-10",
-        "tags": [
-            "Folk",
-            "Pop",
-            "Rock"
-        ]
+        "artist": "Dylan, Bob"
       },
       {
         "id": 68158,
         "title": "There's A Light That Never Goes Out",
-        "artist": "Smiths, The",
-        "language": "English",
-        "popularity": 4010,
-        "play_count": 862,
-        "added_on": "2011-06-29",
-        "tags": [
-            "Alternative",
-            "Indie",
-            "Pop",
-            "Rock",
-            "80s",
-            "British"
-        ]
+        "artist": "Smiths, The"
       }
     ]
     const action = actions.setSongs(songs);
