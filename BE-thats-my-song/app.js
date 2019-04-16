dc32bbcec71c34519c26be1eb2dc1bfc259fbce2
“Kimberly <kimberly.achkio@gmail.com>
2019-04-15 20:02:25 -0600

    Begin functionality for deleting song from app.locals.playlist

--- BE-thats-my-song/app.js
+++ BE-thats-my-song/app.js
@@ -24,4 +24,20 @@ app.post('/api/v1/playlist/', (request, response) => {
   //send back the proper response code with the newPlaylistItem(why this? Could we send a message saying "Jingle Bell Rock has successfully been added to your plaslist!")
 });
 
+app.delete('/api/v1/playlist/:id', (request, response) => {
+  // const { id } = request.params;
+  console.log(request.params)
+  const { playlist } = app.locals;
+  console.log(app.locals.playlist);
+  const songIndex = playlist.findIndex(song => song.id == request.params.id);
+  // if(songIndex === -1) {
+  //   return response.status(404).json('Song not found')
+  // };
+
+  console.log(songIndex);
+
+  playlist.splice(songIndex, 1);
+  return response.status(204);
+});
+
  export default app;
