1edb9ccdf3010ffe958353f145734861fe2d4b95
“Kimberly <kimberly.achkio@gmail.com>
2019-04-16 21:29:06 -0600

    Complete test

--- FE-thats-my-song/src/components/PlaylistCard/PlaylistCard.test.js
+++ FE-thats-my-song/src/components/PlaylistCard/PlaylistCard.test.js
@@ -3,8 +3,19 @@ import { shallow } from 'enzyme';
 import PlaylistCard from './PlaylistCard';
 
 describe('PlaylistCard', () => {
+  let wrapper;
+  let mockDeleteSongFromPlaylist;
+  beforeEach(() => {
+    mockDeleteSongFromPlaylist = jest.fn()
+    wrapper = shallow(<PlaylistCard deleteSongFromPlaylist={mockDeleteSongFromPlaylist}/>);
+  });
+
   it('should match the snapshot', () => {
-    const wrapper = shallow(<PlaylistCard />);
     expect(wrapper.debug()).toMatchSnapshot();
+  });
+
+  it('should call props function deleteSongFromPlaylist on click', () => {
+    wrapper.find('.remove-song-button').simulate('click');
+    expect(mockDeleteSongFromPlaylist).toHaveBeenCalled();
   });
 });
