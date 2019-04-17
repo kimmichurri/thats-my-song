0f43fa5da689aa5a8f733810a6bfe73afede35cc
“Kimberly <kimberly.achkio@gmail.com>
2019-04-16 21:54:17 -0600

    Complete test

--- FE-thats-my-song/src/containers/Playlist/Playlist.test.js
+++ FE-thats-my-song/src/containers/Playlist/Playlist.test.js
@@ -2,12 +2,16 @@ import  { Playlist, mapStateToProps, mapDispatchToProps } from './Playlist';
 import React from 'react';
 import { shallow } from 'enzyme';
 jest.mock('../../thunks/getFromPlaylist');
+jest.mock('../../thunks/deleteSong');
+import { deleteFromPlaylist } from '../../actions';
 
 describe('Playlist', () => {
   let wrapper;
   const mockProps = {
     playlist: [{title: 'a song', artist: 'kim', id: 1}],
-    getFromPlaylist: jest.fn()
+    getFromPlaylist: jest.fn(),
+    deleteSong: jest.fn(),
+    deleteFromPlaylist: jest.fn()
   } 
 
   beforeEach(() => {
@@ -15,12 +19,30 @@ describe('Playlist', () => {
       <Playlist 
         playlist={mockProps.playlist}
         getFromPlaylist={mockProps.getFromPlaylist}
+        deleteSong={mockProps.deleteSong}
+        deleteFromPlaylist={mockProps.deleteFromPlaylist}
       />
     )
   });
 
   it('should match the snapshot', () => {
     expect(wrapper.debug()).toMatchSnapshot();
+  });
+
+  it('should call props function getFromPlaylist with the correct parameters', () => {
+    const url = 'http://localhost:3001/api/v1/playlist/';
+    wrapper.instance().goToPlaylist();
+    expect(wrapper.instance().props.getFromPlaylist).toHaveBeenCalledWith(url);
+  });
+
+  it('deleteSongFromPlaylist should call props functions deleteSong and deleteFromPlaylist with correct parameters', () => {
+    const mockEvent = { target: { value: '4'} };
+    const id = '4';
+    const numericId = 4;
+    const url = `http://localhost:3001/api/v1/playlist/4`;
+    wrapper.instance().deleteSongFromPlaylist(mockEvent);
+    expect(wrapper.instance().props.deleteSong).toHaveBeenCalledWith(url, id);
+    expect(wrapper.instance().props.deleteFromPlaylist).toHaveBeenCalledWith(numericId);
   });
 
   describe('mapStateToProps', () => {
@@ -45,6 +67,23 @@ describe('Playlist', () => {
       const actionToDispatch = getFromPlaylist('www.playlist.com');
       const mappedProps = mapDispatchToProps(mockDispatch);
       mappedProps.getFromPlaylist('www.playlist.com')
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should return deleteSong to dispatch', () => {
+      const mockDispatch = jest.fn();
+      const deleteSong = jest.fn();
+      const actionToDispatch = deleteSong('www.delete.com', '4');
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.deleteSong('www.delete.com', '4');
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should return deleteFromPlaylist to dispatch', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = deleteFromPlaylist(4);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.deleteFromPlaylist(4);
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
     });
   });
