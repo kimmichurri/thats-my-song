e04f1ccf0b81b9bb9d17a3a97914fc3afcde21e2
“Kimberly <kimberly.achkio@gmail.com>
2019-06-24 15:27:51 -0600

    Write GET test for playlist

---
+++ API-thats-my-song/spec/requests/playlist_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Playlist API', type: :request do
+  describe 'Get all songs from playlist' do
+    it 'should return all songs stored in the playlist' do
+      song1 = Playlist.create(title: 'I want it that way', artist: 'Backstreet Boys')
+      song2 = Playlist.create(title: 'Bye Bye Bye', artist: '*NSYNC')
+
+      get '/api/v1/playlists'
+      playlists = JSON.parse(request.body)
+
+      expect(response.status).to eq(200)
+      expect(playlists).to be_a(Array)
+      expect(playlists[0].title).to eq(song1.title)
+    end
+  end
+end
